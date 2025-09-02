906eb3466b817709eb813c2a1ef515b445823fd5
Mayank <00mayanks11@gmail.com>
2025-09-02 22:00:32 +0530

    button size change

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -57,7 +57,7 @@ const Todo = () => {
       </div>
 
       {/* input box */}
-      <div className="flex items-center my-7 bg-gray-200 rounded-full">
+      <div className="w-full flex items-center my-7 bg-gray-200 rounded-full">
         <input
           ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
@@ -66,7 +66,7 @@ const Todo = () => {
         />
         <button
           onClick={add}
-          className="border-none rounded-full bg-orange-600 w-28 md:w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          className="text-nowrap px-2 border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
         >
           ADD +
         </button>
