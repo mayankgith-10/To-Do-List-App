6098c5abb1cc5bd7a202fd59654a5188da22be01
Mayank <00mayanks11@gmail.com>
2025-09-02 20:13:09 +0530

    Lists width changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -49,7 +49,7 @@ const Todo = () => {
   }, [todoList]);
 
   return (
-    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+    <div className="bg-white place-self-center w-[345px] md:w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* title */}
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={image} alt="" />
