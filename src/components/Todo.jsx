9f885602728f2ec1356275d38050f59bf5c31672
Mayank <00mayanks11@gmail.com>
2025-09-02 22:28:57 +0530

    One final change

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -49,7 +49,7 @@ const Todo = () => {
   }, [todoList]);
 
   return (
-    <div className="bg-white place-self-center md:w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+    <div className="bg-white place-self-center md:w-11/12 max-w-md flex flex-col p-4 md:p-7 min-h-[550px] rounded-xl">
       {/* title */}
       <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={image} alt="" />
@@ -60,13 +60,13 @@ const Todo = () => {
       <div className="w-full flex items-center my-7 bg-gray-200 rounded-full">
         <input
           ref={inputRef}
-          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          className="bg-transparent border-0 outline-none w-full h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
         <button
           onClick={add}
-          className="text-nowrap px-2 border-none rounded-full bg-orange-600 w-fit md:w-32 h-14 text-white text-lg font-medium cursor-pointer"
+          className="text-nowrap px-2 border-none rounded-full bg-orange-600 w-fit md:w-32 h-14 text-white md:text-lg text-sm font-medium cursor-pointer"
         >
           ADD +
         </button>
